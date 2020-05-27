64ce1c6c8ebdd911d88e66a6ab4004d914ce23cf
Kousika <kousika@squashapps.com>
2020-05-28 02:52:29 +0530

    feat(search): Implemented search by countries functionality

--- src/components/Dashboard/action.js
+++ src/components/Dashboard/action.js
@@ -1,8 +1,21 @@
-import axios from 'axios';
-import { GET_STAT_SUCCEEDED } from './actionTypes.js';
+import axios from "axios";
+import { GET_STAT_SUCCEEDED, SEARCH } from "./actionTypes.js";
 
 export const getStats = () => (dispatch) => {
-    axios.get('https://api.covid19api.com/summary').then((res) => {
-        dispatch({ type: GET_STAT_SUCCEEDED, payload: res.data })
-    });
-}
+  axios.get('https://api.covid19api.com/summary').then((res) => {
+      dispatch({ type: GET_STAT_SUCCEEDED, payload: res.data })
+  });
+};
+
+export const search = (searchTerm) => (dispatch, getState) => {
+  const state = getState();
+  const searchResult = [];
+  state.dashboard.countries.map((country) => {
+    const countryName = country.Country.toLowerCase();
+    if (countryName.includes(searchTerm.toLowerCase())) {
+      searchResult.push(country);
+    }
+    return country;
+  });
+  dispatch({ type: SEARCH, payload: searchResult });
+};
